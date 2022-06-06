a59f58707e3e882f620390d9eacc95402162d1a0
unknown <leonumbela@gmail.com>
2022-06-06 12:50:37 -0300

    Se realiza 1 ajuste

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ taxAmount(subTotal, gap);
 //Calculo total de operacion
 const totalAmount = (A,B) => {
     total += A + B;
-    alert("El total de la operacion es de: "+ total + "- Euros");
+    alert("El monto total de la operacion es de: "+ total + "- Euros");
 }
 totalAmount(subTotal, tax);
 
@@ -146,48 +146,5 @@ alert("Detalle total de la operacion:"+
     "\nMonto total de la operacion: "+total+"- Euros" )
 }
 brief();
-
-
-
-// if ((entrada >= 0) && ( entrada <= 1000)) {
-//     alert("Su presupuesto es bajo");
-// } else if((entrada >= 1001) && ( entrada <= 7500)) {
-//     alert("Su presupuesto es intermedio");
-// } else if(entrada >= 7501) {
-//     alert("Su presupuesto es Alto");
-// }
-// let respuesta = prompt("Desea Ingresar mas efectivo");
-// if((respuesta == "Si")||(respuesta == "si")){
-//     let entrada2 = parseInt(prompt("Ingrese el monto que quiere agregar a su presupuesto"));
-//     let presup = entrada + entrada2;
-//     alert("Su nuevo presupuesto es de: "+ presup+"- pesos");
-
-//     alert("Cuenta con 5 opciones en funcion de su presupuesto")
-//     let presup2 = prompt("Ahora que conoce su  presupuesto actualizado selecione la opcion correspondiente:" + 
-//                         "\n Enter: en caso de no contar con suficiente presupuesto"+
-//                         "\n Box1- Desde 1000 pesos "+
-//                         "\n Box2- Desde 5000 pesos."+
-//                         "\n Box3- Mas de 7500 pesos"+
-//                         "\n Esc");
-//             switch(presup2){
-//                 case "Box1":
-//                 alert("Caja de chocolates");
-//                 break;
-//                 case "Box2":
-//                 alert("Caja de vino con 2 copas");
-//                 break;
-//                 case "Box3":
-//                 alert("Ramo de rosas con un champagne");
-//                 break;
-//                 case "Esc":
-//                 alert("Operacion Cancelada");
-//                 break;
-//                 default:
-//                 alert("Lo sentimos su presupuesto no alcanza para nada");
-//             }
-//             alert("Coordinaremos el envio, gracias.");
-// }else{
-//         alert("Use su imaginacion, la va a necesitar....")
-//     }
 
 
